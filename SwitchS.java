146a2b95dbcbb28bfb6a16f3acc2653f5a3ac2d0
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-05-26 12:09:56 +0530

    small changes

---
+++ SwitchS.java
@@ -0,0 +1,15 @@
+public class SwitchS{
+    public static void mai  (String args[]){
+        int number = 2;
+
+        switch (number){
+            case 1 : System.out.println("momos");
+            break;
+            case 2 :  System.out.println("kfc");
+            break;
+            case 3: System.out.println("pizza");
+            break;
+            default : System.out.println("nothing");
+        }
+    }
+}
