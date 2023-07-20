3884ce32c92f6ca0ac7e1b5de27a3f5f38851381
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-07-20 12:11:56 +0530

    small changes

---
+++ TernaryPassFail.java
@@ -0,0 +1,9 @@
+public class TernaryPassFail{
+    public static void main (String args[]){
+        int marks=67;
+        String result = (marks>=33)?"pass":"fail";
+        System.out.println(result);
+
+
+    }
+}
