3759674b47bed5c10e0d2123c9dcdd2d12929278
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-08-17 11:35:37 +0530

    small changes

---
+++ Avgof3.java
@@ -0,0 +1,10 @@
+public class Avgof3 {
+    public static int Average(int a, int b, int c){
+        
+        return((a+b+c)/3);
+    }
+    public static void main (String args[]){
+        System.out.println(Average(1, 2, 3));
+    }
+    
+}
