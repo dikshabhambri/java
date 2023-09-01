5c2167df5acfa82138d472cc660e6a194a797bcd
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-01 11:43:17 +0530

    small changes

---
+++ FactorialFn.java
@@ -0,0 +1,15 @@
+public class FactorialFn {
+    public static int Factorial (int n){
+        int f=1;
+        for (int i=1; i<=n; i++){
+            f = f*i;
+        
+
+        }
+        return f;
+        public static void main (String args[]){
+           
+            System.out.println(Factorial(4));
+        }
+        }
+    }
