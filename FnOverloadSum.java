ab18b18736ad1b74bed3ebe90731ab87bf44cc78
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-04 11:44:33 +0530

    small changes

---
+++ FnOverloadSum.java
@@ -0,0 +1,15 @@
+public class FnOverloadSum {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+public static int sum(int a, int b, int c){
+    return a+b+c;
+}
+
+public static void main (String args[]){
+    System.out.println(sum(3,5));
+    System.out.println(sum(6,2));
+    
+}
+}
