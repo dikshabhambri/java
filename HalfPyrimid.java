8c63e61a714654923de0678408b5bc8ac7b67328
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-06 11:46:04 +0530

    small changes

---
+++ HalfPyrimid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class HalfPyrimid {
+    public static void main (String args[]) {
+        int n=4;
+
+        for(int line=1; line<=n; line++){
+            for(int number=1; number<=line; number++){
+                System.out.print(number);
+
+            }
+            System.out.println();
+        }    
+        
+    }
+}
