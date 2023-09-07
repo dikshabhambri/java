bd1391e8bb28965fc1b87bf0c9dc499602f11e11
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-07 11:47:14 +0530

    small changes

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse{
+    public static void main (String args[]){
+        int age=16;
+        if(age>=18){
+            System.out.println("Can Drive");
+        }
+        else{
+            System.out.println("Cannot drive");
+        }
+
+    }
+}
