7a64aa666ca2ba35c1e40c80d7a3f80672045e81
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-09 11:48:44 +0530

    small changes

---
+++ InvertedStars.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class InvertedStars {
+public static void main(String args[]){
+    int n =4;
+for (int i=1; i<=4; i++){
+    for (int s=1; s<=n-i+1; s++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+}
+}
