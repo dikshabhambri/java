8d90118a97089e03a6c85088b36e80016f3ba1cc
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-14 11:50:51 +0530

    small changes

---
+++ Largestof2.java
@@ -0,0 +1,13 @@
+public class Largestof2{
+    public static void main (String args[]){
+        int A=10;
+        int B=20;
+        if (A>=B){
+            System.out.println("A is larger");
+
+        }
+        else {
+            System.out.println("B is larger");
+        }
+    }
+}
