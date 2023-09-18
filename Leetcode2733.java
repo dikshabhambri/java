03437af1fca068c5496d7fc7d2a67fb71339f924
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-18 11:52:16 +0530

    small changes

---
+++ Leetcode2733.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Leetcode2733 {
+    public int findNonMinOrMax(int[] nums) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(nums);
+        
+
+        }
+ 
+    }
