d2f0f741e30aa4f9e52cd1c1b6f91004de23f834
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-09-22 11:54:12 +0530

    small changes

---
+++ Me.java
@@ -0,0 +1,12 @@
+public class Me {
+    public static int Sumsd(){
+        int n1=2; int n2=4;
+        
+        System.out.println(n1+n2);
+        return n1+n2;
+    }
+    public static void main(String args[]){
+        
+        Sumsd();
+    }
+}
