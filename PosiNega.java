d5ed4cffdaeeba68bd181a30f92892943e9c3ec0
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-10-03 11:59:07 +0530

    small changes

---
+++ PosiNega.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class PosiNega {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter number:");
+        int number = sc.nextInt();
+        
+        if (number<0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is positive");
+        }
+
+        
+         
+
+    }
+}
