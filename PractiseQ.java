bdeebf031d6556a8cf7e942f8baeae21cdf1ec8b
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-10-04 11:59:40 +0530

    small changes

---
+++ PractiseQ.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class PractiseQ {
+    public static void main (String argd[]){
+        Scanner sc = new Scanner(System.in);{
+            int number1;
+            int choice;
+            int evenSum;
+            int oddSum;
+            System.out.println("Enter your number");
+            number1 sc = sc.nextInt();
+
+            if (number1%2==0){
+                evenSum += number1;
+
+            } else{
+                oddSum = oddSum += number1;
+            
+            }
+            System.out.println("The sum of odd numbers is:" + oddSum);
+            System.out.println("The sum of even numbers is:" + evenSum);
+}
+    }
+}
