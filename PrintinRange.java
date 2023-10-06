fc77672ab3d60e3abff4e63197fe6ad61da5a94b
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-10-06 12:00:20 +0530

    small changes

---
+++ PrintinRange.java
@@ -0,0 +1,27 @@
+public class PrintinRange {
+    public static boolean Isprime(int n){
+        for (int i=2; i<=Math.sqrt(n); i++){
+            if (n%i==0){
+                return false;
+
+            }
+            
+        }
+        return true;
+    }
+    public static void PrintinRange(int n){
+        for(int i=2; i<=n; i++){
+            if(Isprime(i)){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void main (String args[]){
+        PrintinRange(20);
+
+    }
+    
+}
