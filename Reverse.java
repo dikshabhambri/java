7504be5b339b71f0ce1e7ac33efca3bc1eee6170
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-10-13 12:03:29 +0530

    small changes

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+public class Reverse{
+    public static void main(String args[]){
+        int n = 10899;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            
+        
+        System.out.println(lastDigit + " " );
+        n = n/10;
+        }
+        System.out.println();
+    }
+}
