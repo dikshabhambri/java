991f6de6b26f3800e23d958b3adac9263ab09de2
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-10-14 12:04:10 +0530

    small changes

---
+++ reverseArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class SwapArray {
+    public static int reverseArray(int numbers[]){
+        int start = 0, end= numbers.length - 1;
+        while (start<end){
+        int temp = numbers[end];
+        numbers[end] = numbers[start];
+        numbers[start]= temp;
+        start++;
+        end--;
+
+    }
+}
+    public static void main (String args[]){
+        int numbers[] = {2,4,6,8,10};
+        reverseArray(numbers);
+        for (int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i]+ " ");
+
+        }
+    }
+}
