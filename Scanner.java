e75a167d3d675211156a32a1b62a5377ebb46e86
dikshabhambri <diksha.bhambri2020@vitbhopal.ac.in>
2023-10-17 12:05:03 +0530

    small changes

---
+++ Scanner.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ScannerTry {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        Boolean bn = sc.nextBoolean();
+        System.out.println(value);
+    }
+}
